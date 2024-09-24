0ce10e2fd458662881a73dafe68e727535609a2b
Juanxpeke <juanintendo.f11@gmail.com>
2024-09-24 17:10:17 -0300

    Added Weekend chapter 3 code

---
+++ Source/Color.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "Vec3.h"
+
+#include <iostream>
+
+using Color = Vec3;
+
+void WriteColor(std::ostream& out, const Color& pixelColor)
+{
+  auto r = pixelColor.X();
+  auto g = pixelColor.Y();
+  auto b = pixelColor.Z();
+
+  // Translate the [0,1] component values to the byte range [0,255]
+  int rByte = int(255.999 * r);
+  int gByte = int(255.999 * g);
+  int bByte = int(255.999 * b);
+
+  // Write out the pixel color components
+  out << rByte << ' ' << gByte << ' ' << bByte << '\n';
+}
+
+#endif // COLOR_H
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 
+#include "Color.h"
+#include "Vec3.h"
+
 int main() {
-  // Image
-  int image_width = 256;
-  int image_height = 256;
 
-  // Render
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    // Image
 
-  for (int j = 0; j < image_height; j++)
-  {
-    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+    int image_width = 256;
+    int image_height = 256;
 
-    for (int i = 0; i < image_width; i++)
-    {
-      auto r = double(i) / (image_width-1);
-      auto g = double(j) / (image_height-1);
-      auto b = 0.0;
+    // Render
 
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            auto pixel_color = Color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            WriteColor(std::cout, pixel_color);
+        }
     }
-  }
 
-  std::clog << "\rDone.                 \n";
+    std::clog << "\rDone.                 \n";
 }
